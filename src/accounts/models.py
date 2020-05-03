7686cf594938debfb90553b93a844b540820e777
DarkKnight1990 <pikludey1990@gmail.com>
2020-05-03 16:35:47 +0530

    add organization model

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from django_countries.fields import CountryField
+
+from common.models import AbstractTimeStamp
+
+
+class Organization(AbstractTimeStamp):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    domain = models.CharField(max_length=100)
+    countries = CountryField(multiple=True)
+    parent = models.ForeignKey(
+        'self', verbose_name=_('Parent Organization'),
+        blank=True, null=True, on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('organization')
+        verbose_name_plural = _('organizations')
