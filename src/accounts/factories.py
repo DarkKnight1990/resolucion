2e068f27195d629123de3944f5193f17badcce58
DarkKnight1990 <pikludey1990@gmail.com>
2020-05-09 20:30:44 +0530

    added factory method to generate CustomUser and User Profile

--- src/accounts/factories.py
+++ src/accounts/factories.py
@@ -1,10 +1,8 @@
 import factory
 
-from django_countries.fields import Country
-from django_countries.tests.models import MultiCountry
 from faker import Factory
 
-from accounts.models import Organization
+from accounts.models import CustomUser, Organization, UserProfile
 from common.utils import generate_unique_slug
 
 faker = Factory.create()
@@ -15,8 +13,37 @@ class OrganizationFactory(factory.DjangoModelFactory):
     class Meta:
         model = Organization
         django_get_or_create = ('slug',)
-    
-    name        = factory.LazyAttribute(lambda o: faker.company())
+
+    name = factory.LazyAttribute(lambda o: faker.company())
     description = factory.LazyAttribute(lambda o: faker.text())
-    slug        = factory.LazyAttribute(lambda o: generate_unique_slug(Organization(), o.name))
-    domain      = factory.LazyAttribute(lambda o: "www." + faker.domain_name())
+    slug = factory.LazyAttribute(
+        lambda o: generate_unique_slug(Organization(), o.name)
+    )
+    domain = factory.LazyAttribute(lambda o: "www." + faker.domain_name())
+
+
+class CustomUserFactory(factory.DjangoModelFactory):
+
+    class Meta:
+        model = CustomUser
+        django_get_or_create = ('email',)
+
+    email = factory.LazyAttribute(lambda o: faker.email())
+    first_name = factory.LazyAttribute(lambda o: faker.first_name())
+    last_name = factory.LazyAttribute(lambda o: faker.last_name)
+    password = factory.LazyAttribute(lambda o: faker.password())
+
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        manager = cls._get_manager(model_class)
+        return manager.create_user(*args, **kwargs)
+
+
+class UserProfileFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = UserProfile
+        django_get_or_create = ('user',)
+
+    user = factory.SubFactory(CustomUserFactory)
+    organization = factory.SubFactory(OrganizationFactory)
+    contact_number = factory.LazyAttribute(lambda o: faker.msisdn())
